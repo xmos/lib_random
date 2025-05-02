a42dee7c2234f29db8de26ac1f57fe0c4aa6d388
Henk Muller <henk@xmos.com>
2025-05-02 11:50:51 +0100

    Copyright notice update

--- lib_random/src/random_internal.h
+++ lib_random/src/random_internal.h
@@ -1,4 +1,4 @@
-// Copyright 2025-2025 XMOS LIMITED.
+// Copyright 2024-2025 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #ifndef __random_internal_h__
